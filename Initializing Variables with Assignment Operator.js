910dc853cd8648f0c4b66b7b2c45135d070fd316
Jermaine T. Lennon <jermainetlennon2@gmail.com>
2022-10-01 15:22:02 -0400

    Add files via upload

---
+++ Initializing Variables with Assignment Operator.js
@@ -0,0 +1,31 @@
+// Initializing Variables with Assignment Operator
+
+
+// Initialize these three variables
+
+var a = 23;
+var b = 100;
+var c = "My name is";
+
+// Does not change code below this line 
+
+
+a = a + 17;
+
+b = (a + b) * 4;
+
+c = c + " Jermaine T. Lennon";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Case Sensitivity in Variables
+
+// Declare variables using camel case:
+
+var myNameIs;
+
+myNameIs = "Jermaine T. Lennon";
+
+console.log("My name is ", myNameIs)
