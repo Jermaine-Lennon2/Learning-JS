735a9843420a44fe13414ea9e526508f5dc14fd4
Jermaine T. Lennon <jermainetlennon2@gmail.com>
2022-10-01 15:08:02 -0400

    Add files via upload (#1)

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+// DataTypes:
+
+// undefined
+// null = nothing
+// boolean = True/False
+
+// To datatype: use var for full program
+
+var myName = "Jermaine"
+
+// use let for scope where you declare that
+
+let ourName = " Lennon "
+
+// Const cant not change
+ 
+const age = 23.0
